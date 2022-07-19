541b47333207782baff0b8f53a434812ff20298b
pawel.awramiuk <pawel.awramiuk@britenet.com.pl>
2022-07-19 13:54:42 +0200

    Początek AuthenticationService - wzięte z githuba
    Signed-off-by: pawel.awramiuk <pawel.awramiuk@britenet.com.pl>

--- src/main/java/pl/britenet/spring/campusbritenetspring/service/AuthenticationService.java
+++ src/main/java/pl/britenet/spring/campusbritenetspring/service/AuthenticationService.java
@@ -1,16 +1,49 @@
 package pl.britenet.spring.campusbritenetspring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.britenet.campus.models.User;
+import pl.britenet.campus.services.UserService;
+import pl.britenet.spring.campusbritenetspring.model.UserCredentials;
+import pl.britenet.spring.campusbritenetspring.model.UserLoginData;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class AuthenticationService {
+    private final Map<String, Integer> activeTokens;
+    private final UserService userService;
 
-    public void login() {
-
+    @Autowired
+    public AuthenticationService(UserService userService) {
+        this.activeTokens = new HashMap<>();
+        this.userService = userService;
     }
 
-    public Boolean logout() {
-        return false;
+    public UserLoginData login(UserCredentials userCredentials) {
+        Optional<User> oUser = this.userService.getUser(userCredentials.getEmail(), userCredentials.getPassword());
+        if (oUser.isPresent()) {
+            int userId = oUser.get().getId();
+            String userToken = UUID.randomUUID().toString();
+            this.activeTokens.put(userToken, userId);
+            return new UserLoginData(userId, userToken);
+        }
+        else {
+            throw new IllegalStateException("Invalid credentials.");
+        }
     }
 
-    public void register() {
+    public boolean isLogged(String userToken) {
+        return this.activeTokens.containsKey(userToken);
+    }
 
+    public void register(UserCredentials userCredentials) {
+        User user = new User();
+        user.setEmail(userCredentials.getEmail());
+        user.setPassword(userCredentials.getPassword());
+        this.userService.insertUser(user);
     }
 }
